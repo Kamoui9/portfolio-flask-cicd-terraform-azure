efff96b2865456f2500c65f13f2b6f2a4037b40a
Kamoui <pannetier.mateo@gmail.com>
2024-08-18 19:52:12 +0000

    corrected formatting (E501 complience)

--- Models/projectModel.py
+++ Models/projectModel.py
@@ -6,7 +6,8 @@ class ProjectModel:
     Attributes:
         data (list): A list containing project data.
     Methods:
-        __init__(self, filepath: str) -> None: Initializes the ProjectModel object by loading project data from a file.
+        __init__(self, filepath: str) -> None: Initializes the ProjectModel object
+        by loading project data from a file.
 
         getProject(self, id=None): Retrieves a project based on its ID.
     """
@@ -18,9 +19,11 @@ class ProjectModel:
     def getProject(self, id=None):
         """Retrieves a project based on its ID.
         Args:
-            id (int, optional): The ID of the project to retrieve. If not provided, returns all projects.
+            id (int, optional): The ID of the project to retrieve.
+            If not provided, returns all projects.
         Returns:
-            dict or list: The project with the specified ID if found, or all projects if ID is not provided.
+            dict or list: The project with the specified ID if found,
+            or all projects if ID is not provided.
         """
         if isinstance(id, int):
             for project in self.data:
